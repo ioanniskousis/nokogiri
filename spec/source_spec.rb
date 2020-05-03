b8908a10ba11034ceaa8d8069dbe1b37dd6f5bff
Ioannis Kousis <bonobo@otenet.gr>
2020-05-03 14:00:48 +0300

    test source

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -5,18 +5,22 @@ RSpec.describe Source do
   newsweek_hash = {}
   newsweek_hash.extend(NewsWeek)
 
-  describe '#initialize' do
+  describe '#NewsWeek' do
     it 'returns true newsweek_hash responds to method "setup" ' do
       expect(newsweek_hash.respond_to?(:setup)).to be true
     end
   end
 
-  # newsweek_hash.setup
-  # let(:source) { Source.new() }
-
-  # describe '#new instance' do
-  #   it 'returns true if \"FEATURED STORIES\" is found in the page\'s sections' do
-
-  #   end
-  # end
+  newsweek_hash.setup
+  source = Source.new(newsweek_hash)
+  source.open
+  
+  describe '#Source' do
+    it 'returns true if FEATURED STORIES is found in the page sections' do
+      expect(source.sections.any? { |section| 
+      puts section.title
+      section.title.upcase == 'FEATURED STORIES' 
+      }).to be true
+    end
+  end
 end
